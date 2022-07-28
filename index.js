29a51d7ef6181bb901e1e7a7967197882090ce94
Mohammad Alwazzan <kingwazzan2@gmail.com>
2022-07-29 01:41:19 +0300

    the entire css was changed
    the html changed as weel to contain a header and a footer the javascript edited as well still working on the first to reach 5 wins

--- index.js
+++ index.js
@@ -1,87 +1,85 @@
-let you = 0;                                                                    //here are the keep score variables 
+let player = 0 ;
 let computer = 0;
-const content = document.querySelector('#content');
-
-const win = document.createElement('div');
-win.classList.add('win');
-
-const score = document.createElement('div');
-score.classList.add('score');
-
-const result = document.createElement('div');
-result.classList.add('result');
+let player1Selection;
+// a function to return one of three choices 
 
 
-function computerPlay(){
-let rand = ['rock', 'paper', 'scissor']
-let random = rand[Math.floor(Math.random() * rand.length)];
-return random;
+function getComputerChoice(){
+    const choice = ['paper', 'rock', 'scissor'];
+    return choice[Math.floor(Math.random()*choice.length)];
 }
 
-const test = (p,c) => {
-    if(you == 5 || computer == 5){
-        if(you == 5){
-            result.textContent="The Winner is You " ;
-            content.appendChild(result);
-        }
-        else{
-            result.textContent="Sorry you lost !" ;
-            content.appendChild(result);
-        }
-        return;
+function updateResults(){
+    // the variables that are needed to update the results 
+    const player1 = document.getElementById('player1');
+    const computerUpdate = document.getElementById('computer');
+    // the update statements for the two players 
+    player1.innerText = player.toString();
+    computerUpdate.innerText = computer.toString();
+}
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection) {
+        updateResults();
+        return `Draw!!! ${playerSelection} equals ${computerSelection}`;  
     }
-    if(p == c){
-        win.textContent='Draw ! '+ p + " equals "+ c;
-        content.appendChild(win);  
-    }
-    else if(playRound(p,c)){
-        win.textContent='You win ! '+ p + " beats " + c;
-        content.appendChild(win);
-
-        ++you;
+    switch(playerSelection){
+        case 'paper':{
+            switch(computerSelection){
+                case 'rock': {
+                    ++player;
+                    updateResults();
+                    return `player1 win's !!! ${playerSelection} beats ${computerSelection}`;
+                }
+                default:{
+                    ++computer;
+                    updateResults();
+                    return `computer win's !!! ${computerSelection} beats ${playerSelection}`;
+                }
+            }
         }
-    else{
-        win.textContent='You lose ! '+ c + " beats " + p;
-        content.appendChild(win);
-      
-        ++computer;
-          }
-    score.textContent= "The Score: You = "+you+", Computer = "+computer;
-        content.appendChild(score);
-};
+        case'rock':{
+            switch(computerSelection){
+                case 'scissor': {
+                    ++player;
+                    updateResults();
+                    return `player1 win's !!! ${playerSelection} beats ${computerSelection}`;
+                }
+                default:{
+                    ++computer;
+                    updateResults();
+                    return `computer win's !!! ${computerSelection} beats ${playerSelection}`;
+                }
+            }
+
+        }
+        default:{
+            switch(computerSelection){
+                case 'paper': {
+                    ++player;
+                    updateResults();
+                    return `player1 win's !!! ${playerSelection} beats ${computerSelection}`;
+                }
+                default:{
+                    ++computer;
+                    updateResults();
+                    return `computer win's !!! ${computerSelection} beats ${playerSelection}`;
+                }
+            }
+        }
+    }
+}
+document.getElementById('paper').addEventListener('click',() => {play('paper')});
+document.getElementById('rock').addEventListener('click',()=>{play('rock')});
+document.getElementById('scissor').addEventListener('click',() =>{play('scissor')});
 
 
-
-let playRound = function(playerSelection, computerSelection){
-if(playerSelection == 'rock' && computerSelection == 'scissor'){return true;}
-else if(playerSelection == 'rock' && computerSelection == 'paper'){return false;}
-else if(playerSelection == 'scissor' && computerSelection == 'paper'){return true;}
-else if(playerSelection == 'scissor' && computerSelection == 'rock'){return false;}
-else if(playerSelection == 'paper' && computerSelection == 'rock'){return true;}
-else{return false;}
-};
-
-const btn1 = document.querySelector('#paper');
-btn1.addEventListener('click',()=>{
-    const player = 'paper';
-    const comp = computerPlay();
-    test(player,comp);
-   
-});
-
-const btn2 = document.querySelector('#rock');
-btn2.addEventListener('click',()=>{
-    const player = 'rock' ;
-    const comp = computerPlay();
-    test(player,comp);
-
-});
-
-const btn3 = document.querySelector('#scissor');
-btn3.addEventListener('click',()=>{
-    const player = 'scissor';
-    const comp = computerPlay();
-    test(player,comp);
-});
+function play(choice){
+    const player = choice;
+    const comp = getComputerChoice();
+    if(player==5)
+    playRound(player,comp);
+  //  checkWinner();
+}
 
 
